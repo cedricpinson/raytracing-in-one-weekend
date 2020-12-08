0ef84090f966f70083b1d60733f8a7050086dec2
trigrou <trigrou@trigrou.com>
2020-12-08 21:40:36 +0100

    Adjust the code to handle materials

--- raytracing.glsl
+++ raytracing.glsl
@@ -50,6 +50,36 @@ float random(){
 
 const float pi = 3.1415926535897932385;
 
+
+// vec4 vec3 shape position + index properties
+// properties
+const int ShapeSphere = 0;
+struct Item
+{
+    vec3 position;
+    int material;  // material index
+    int shapeType; // 0 sphere
+    float radius;
+};
+
+const int MaterialLambert = 0;
+const int MaterialMetal = 1;
+struct Material
+{
+    // 0  = Lambert material
+    // 1  = Metal material
+    int type;
+    vec3 albedo;
+};
+
+struct Scene
+{
+    Item items[10];
+    Material materials[10];
+    int numItems;
+
+} gScene;
+
 struct Ray {
     vec3 origin;
     vec3 direction;
@@ -59,6 +89,7 @@ struct Hit {
     vec3 position;
     float t;
     vec3 normal;
+    int material;
 };
 
 vec3 rayAt(Ray ray, float t) {
@@ -98,7 +129,8 @@ vec3 randomUnitHemiSphere(inout int seed, const vec3 normal) {
     return normalize(-p);
 }
 
-bool intersectSphere(const Ray ray, float t_min, float t_max, const vec4 sphere, out Hit hit) {
+bool intersectSphere(const Ray ray, float t_min, float t_max, const Item item, out Hit hit) {
+    vec4 sphere = vec4(item.position.xyz, item.radius);
     vec3 oc = ray.origin - sphere.xyz;
     float a = dot(ray.direction, ray.direction);
     float b = 2.0 * dot(oc, ray.direction);
@@ -154,22 +186,19 @@ vec3 rayBackgroundColor(const Ray r) {
     return mix(ground, sky, t);
 }
 
-vec4 worlds[2] = vec4[](
-                        vec4(0.0,0.0,-1.0,0.5),
-                        vec4(0.0,-100.5,-1.0,100.0));
-const int numItems = 2;
-
 
 bool intersectWorld(const Ray ray, float min_t, float max_t, out Hit hit)
 {
     int i;
     bool rayHit = false;
-    for ( i = 0 ; i < numItems; i++) {
-        vec4 sphere = worlds[i];
-        bool intersect = intersectSphere(ray, min_t, max_t, sphere, hit);
-        if (intersect) {
-            max_t = hit.t;
-            rayHit = true;
+    for ( i = 0 ; i < gScene.numItems; i++) {
+        if (gScene.items[i].shapeType == ShapeSphere) {
+            Item sphere = gScene.items[i];
+            bool intersect = intersectSphere(ray, min_t, max_t, sphere, hit);
+            if (intersect) {
+                max_t = hit.t;
+                rayHit = true;
+            }
         }
     }
 
@@ -177,56 +206,108 @@ bool intersectWorld(const Ray ray, float min_t, float max_t, out Hit hit)
 }
 
 int raySeed = 0;
-bool rayInner(const Ray ray, out Ray nextRay, out Hit hit)
+
+// Lambert material scattering
+bool scatterLambert(const Ray ray, const Hit hit, out vec3 attenuation, out Ray scattered) {
+    scattered.direction = hit.normal + randomUnitSphere(raySeed);
+    if (abs(scattered.direction.x) < 1e-8 && abs(scattered.direction.y) < 1e-8 && abs(scattered.direction.z) < 1e-8) {
+        scattered.direction = hit.normal;
+    }
+    scattered.origin = hit.position;
+    attenuation = gScene.materials[hit.material].albedo;
+    return true;
+}
+// Metal material scattering
+bool scatterMetal(const Ray ray, const Hit hit, out vec3 attenuation, out Ray scattered) {
+    vec3 reflected = reflect(normalize(ray.direction), hit.normal);
+    scattered.direction = reflected;
+    scattered.origin = hit.position;
+    attenuation = gScene.materials[hit.material].albedo;
+    return (dot(scattered.direction, hit.normal) > 0.0);
+}
+
+bool rayInner(const Ray ray, out Ray nextRay, inout vec3 color)
 {
+    Hit hit;
     if (intersectWorld(ray, 0.001, 1e8, hit)) {
-#if 0
-        vec3 target = hit.position + hit.normal + randomUnitSphere(raySeed);
-        nextRay = Ray(hit.position, normalize(target-hit.position));
-#else
-        nextRay = Ray(hit.position, randomUnitHemiSphere(raySeed, hit.normal));
-#endif
-        return true;
+
+        bool result = false;
+        vec3 attenuation;
+        switch (hit.material) {
+        case MaterialLambert:
+            result = scatterLambert(ray, hit, attenuation, nextRay);
+            break;
+        case MaterialMetal:
+            result = scatterMetal(ray, hit, attenuation, nextRay);
+            break;
+        }
+
+        if (result) {
+            color *= attenuation;
+        } else {
+            color *= vec3(0.0);
+        }
+        //nextRay = Ray(hit.position, randomUnitHemiSphere(raySeed, hit.normal));
+        return result;
     }
 
+    color *= rayBackgroundColor(ray);
     return false;
 }
 
 vec3 rayColor(const Ray ray)
 {
-    vec3 col = vec3(0.0);
+    vec3 color = vec3(1.0);
     const int maxNumRay = 10;
-    Hit hits[maxNumRay];
-    int numRays = 0;
     int i = 0;
-    Ray prevRay = ray;
     Ray currentRay = ray;
     Ray nextRay;
     for (i = 0; i < maxNumRay; i++) {
-        bool hit = rayInner(currentRay, nextRay, hits[i]);
+        bool hit = rayInner(currentRay, nextRay, color);
         if (!hit) {
             break;
         }
-        prevRay = currentRay;
         currentRay = nextRay;
     }
 
     if (i==maxNumRay) {
         return vec3(0.0);
     }
-    if (i==0) {
-        return rayBackgroundColor(ray);
-    }
-    //col = 0.5 * (hits[i-1].normal + vec3(1.0));
-    float factor = 1.0/float(1<<(i));
 
-    return factor * rayBackgroundColor(currentRay);
+    return color;
+}
+
+void setupScene(out Scene scene)
+{
+    scene.materials[0].type = MaterialLambert;
+    scene.materials[0].albedo = vec3(0.5,0.5,0.5);
+
+    scene.materials[1].type = MaterialMetal;
+
+
+    int itemIndex = 0;
+    scene.items[itemIndex].position = vec3(0.0, 0.0, -1.0);
+    scene.items[itemIndex].radius = 0.5;
+    scene.items[itemIndex].shapeType = ShapeSphere;
+    scene.items[itemIndex].material = 0;
+    itemIndex++;
+
+    scene.items[itemIndex].position = vec3(0.0,-100.5,-1.0);
+    scene.items[itemIndex].radius = 100.0;
+    scene.items[itemIndex].shapeType = ShapeSphere;
+    scene.items[itemIndex].material = 0;
+    itemIndex++;
+
+    scene.numItems = itemIndex;
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     int rngSeed = int(fragCoord.x) + int(fragCoord.y) * int(iResolution.x);
     raySeed = rngSeed;
+
+    setupScene(gScene);
+
     const int numSamples = 10;
     int i;
     vec3 col= vec3(0.0);
